79b947455d65f4194ab833c88209a7fd7ef8612b
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-12-06 02:37:40 -0500

    finishing up css for displayResults

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./CSS/DisplayResults.css";
 const DisplayResults = props => {
 	const { resData } = props;
 	if (resData === "") return null;
-	
+
 	return resData.map((el, i) => {
 		return (
 			<div key={i} className="row">
@@ -16,19 +16,20 @@ const DisplayResults = props => {
 						<span className="card-title">{el.data.title}</span>
 
 						<div className="card-content">
-							<span>Date Created: {props.timeConverter(el.data.created)}</span>
-							<br />
+							<div className="display-override">
+						<span>Date Created: {props.timeConverter(el.data.created)}</span>
+
 							<span>Subreddit: {el.data.subreddit_name_prefixed}</span>
-							<br />
-							<span>score: {el.data.score}</span>
-							<br />
-							<span>ups: {el.data.ups}</span>
-							<br />
-							<span>comments: {el.data.num_comments}</span>
+
+							<span>Score: {el.data.score}</span>
+
+							<span>Ups: {el.data.ups}</span>
+
+							<span>Comments: {el.data.num_comments}</span>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className="col s1 m1" />
 			</div>
 		);
 	});
